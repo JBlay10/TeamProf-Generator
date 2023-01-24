1d938e0e300f9101f478b77b5e05b98e9bc195cf
JBlay10 <joseblay10@gmail.com>
2023-01-24 16:40:25 -0600

    initial commit

---
+++ src/GenHTML.js
@@ -0,0 +1,17 @@
+const genIntern = function (intern) {
+    return`
+    `;
+}
+
+const genEngineer = function (engineer) {
+    return`
+    
+    `;
+}
+
+const genManager = function (manager) {
+    return`
+    
+    `;
+}
+
